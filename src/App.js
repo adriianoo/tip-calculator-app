e29d6d62ed8aac5ef279b3fd82a888e03be55b49
Adriano Jimenez Hidalgo <adrijimenez@hotmail.com>
2023-08-12 13:57:15 +0200

    addded scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles.scss"
 import PercentageButton from "./components/PercentageButton.js";
 
 const App = () => {
--- src/components/PercentageButton.js
+++ src/components/PercentageButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./PercentageButton.css"
+import "./PercentageButton.scss"
 
 const PercentageButton = (props) => {
     return (
